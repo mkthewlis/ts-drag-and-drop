e16bd1a0e2887aca8b637ba0a23e5940fdbc0b9c
Miranda Thewlis <miranda.thewlis@jayway.com>
2021-06-09 13:43:19 +0200

    feat: adds basic form validation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,19 @@
+// Autobind TS decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor){
+    // Stores the method we originally defined
+    const originalMethod = descriptor.value;
+    // Adjusted descriptor object that binds the property sent in
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
+// ProjectInput Class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -25,11 +41,39 @@ class ProjectInput {
         this.attach();
     }
 
+    // method to gather and validate input data
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+
+        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+            alert("Invalid input, please try again!");
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, parseFloat(enteredPeople)];
+        }
+    }
+
+    // method to clear inputs after from submission
+    private clearInputs() {
+        this.titleInputElement.value = "";
+        this.descriptionInputElement.value = "";
+        this.peopleInputElement.value = "";
+    }
+
     // method to handle form submission
+    @autobind
     private submitHandler(event: Event) {
         // prevent default submission
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        // checks if userInput is an array (as TS tuples - defined in the gatherUserInput method - are)
+        if (Array.isArray(userInput)){
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
     }
 
     // method to configure the form
@@ -44,4 +88,5 @@ class ProjectInput {
     }
 }
 
+// Instance of ProjectInput class
 const prjInput = new ProjectInput();
