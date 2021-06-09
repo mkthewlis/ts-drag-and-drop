d4d6c4f1520c3598ba1dd69942cf3c840d4dd872
Miranda Thewlis <miranda.thewlis@jayway.com>
2021-06-09 13:23:19 +0200

    feat: adds basic logic to create and handle form submission

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
-    element:HTMLFormElement;
+    element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     constructor() {
         // access html elements in index.html file
@@ -11,10 +14,31 @@ class ProjectInput {
         // form element to be rendered to the application
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = "user-input";
+
+        this.titleInputElement = this.element.querySelector("#title")! as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector("#description")! as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector("#people")! as HTMLInputElement;
+
+        // calls the methods below
+        this.configure();
         this.attach();
     }
 
-    // inserts new element into DOM
+    // method to handle form submission
+    private submitHandler(event: Event) {
+        // prevent default submission
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+
+    // method to configure the form
+    private configure() {
+        this.element.addEventListener("submit", this.submitHandler.bind(this));
+    }
+
+
+    // method to attach the form element into DOM
     private attach() {
         this.hostElement.insertAdjacentElement("afterbegin", this.element);
     }
