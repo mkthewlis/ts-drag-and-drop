8aa01a5c38d0c950216cbba42bb2bec1a36d8155
Miranda Thewlis <miranda.thewlis@jayway.com>
2021-06-10 10:08:01 +0200

    refactor: updates to ES modules

--- src/components/base-component.ts
+++ src/components/base-component.ts
@@ -1,49 +1,43 @@
-namespace App {
-  // Component Base Class
-  export abstract class Component<
-    T extends HTMLElement,
-    U extends HTMLElement
-  > {
-    templateElement: HTMLTemplateElement;
-    // T & U used here as the html element differs depending on where Component is used
-    hostElement: T;
-    element: U;
+export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement;
+  // T & U used here as the html element differs depending on where Component is used
+  hostElement: T;
+  element: U;
 
-    constructor(
-      templateId: string,
-      hostElementId: string,
-      insertAtStart: boolean,
-      newElementId?: string
-    ) {
-      // access html elements in index.html file to initialise it
-      this.templateElement = document.getElementById(
-        templateId
-      )! as HTMLTemplateElement;
-      this.hostElement = document.getElementById(hostElementId)! as T;
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    insertAtStart: boolean,
+    newElementId?: string
+  ) {
+    // access html elements in index.html file to initialise it
+    this.templateElement = document.getElementById(
+      templateId
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(hostElementId)! as T;
 
-      // html element to be rendered to the application
-      const importedNode = document.importNode(
-        this.templateElement.content,
-        true
-      );
-      this.element = importedNode.firstElementChild as U;
-      // creates dynamic ids
-      if (newElementId) {
-        this.element.id = newElementId;
-      }
-
-      this.attach(insertAtStart);
+    // html element to be rendered to the application
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as U;
+    // creates dynamic ids
+    if (newElementId) {
+      this.element.id = newElementId;
     }
 
-    // method to attach the form element into DOM
-    private attach(insertAtBeginning: boolean) {
-      this.hostElement.insertAdjacentElement(
-        insertAtBeginning ? 'afterbegin' : 'beforeend',
-        this.element
-      );
-    }
-
-    abstract configure(): void;
-    abstract renderContent(): void;
+    this.attach(insertAtStart);
   }
+
+  // method to attach the form element into DOM
+  private attach(insertAtBeginning: boolean) {
+    this.hostElement.insertAdjacentElement(
+      insertAtBeginning ? 'afterbegin' : 'beforeend',
+      this.element
+    );
+  }
+
+  abstract configure(): void;
+  abstract renderContent(): void;
 }
--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,94 +1,91 @@
-/// <reference path="base-component.ts"/>
-/// <reference path="../decorators/autobind.ts"/>
-/// <reference path="../util/validation.ts"/>
-/// <reference path="../state/project-state.ts"/>
+import { Component } from './base-component.js';
+import { Validatable, validate } from '../util/validation.js';
+import { autobind } from '../decorators/autobind.js';
+import { projectState } from '../state/project-state.js';
 
-namespace App {
-  // ProjectInput Class
-  export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
-    titleInputElement: HTMLInputElement;
-    descriptionInputElement: HTMLInputElement;
-    peopleInputElement: HTMLInputElement;
+export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
-    constructor() {
-      super('project-input', 'app', true, 'user-input');
-      this.titleInputElement = this.element.querySelector(
-        '#title'
-      )! as HTMLInputElement;
-      this.descriptionInputElement = this.element.querySelector(
-        '#description'
-      )! as HTMLInputElement;
-      this.peopleInputElement = this.element.querySelector(
-        '#people'
-      )! as HTMLInputElement;
+  constructor() {
+    super('project-input', 'app', true, 'user-input');
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    )! as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    )! as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    )! as HTMLInputElement;
 
-      this.configure();
-      this.renderContent();
+    this.configure();
+    this.renderContent();
+  }
+
+  // method to configure the form
+  configure() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
+  }
+
+  renderContent() {}
+
+  // method to gather and validate input data
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    // creates objects needed for validation process
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true
+    };
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5
+    };
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople, // people converted to number here with the + sign!
+      required: true,
+      min: 1,
+      max: 5
+    };
+
+    // runs the validation
+    if (
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert('Invalid input, please try again!');
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, parseFloat(enteredPeople)];
     }
+  }
 
-    // method to configure the form
-    configure() {
-      this.element.addEventListener('submit', this.submitHandler.bind(this));
-    }
+  // method to clear inputs after from submission
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
 
-    renderContent() {}
-
-    // method to gather and validate input data
-    private gatherUserInput(): [string, string, number] | void {
-      const enteredTitle = this.titleInputElement.value;
-      const enteredDescription = this.descriptionInputElement.value;
-      const enteredPeople = this.peopleInputElement.value;
-
-      // creates objects needed for validation process
-      const titleValidatable: Validatable = {
-        value: enteredTitle,
-        required: true
-      };
-      const descriptionValidatable: Validatable = {
-        value: enteredDescription,
-        required: true,
-        minLength: 5
-      };
-      const peopleValidatable: Validatable = {
-        value: +enteredPeople, // people converted to number here with the + sign!
-        required: true,
-        min: 1,
-        max: 5
-      };
-
-      // runs the validation
-      if (
-        !validate(titleValidatable) ||
-        !validate(descriptionValidatable) ||
-        !validate(peopleValidatable)
-      ) {
-        alert('Invalid input, please try again!');
-        return;
-      } else {
-        return [enteredTitle, enteredDescription, parseFloat(enteredPeople)];
-      }
-    }
-
-    // method to clear inputs after from submission
-    private clearInputs() {
-      this.titleInputElement.value = '';
-      this.descriptionInputElement.value = '';
-      this.peopleInputElement.value = '';
-    }
-
-    // method to handle form submission
-    @autobind
-    private submitHandler(event: Event) {
-      // prevent default submission
-      event.preventDefault();
-      const userInput = this.gatherUserInput();
-      // checks if userInput is an array (as TS tuples - defined in the gatherUserInput method - are)
-      if (Array.isArray(userInput)) {
-        const [title, desc, people] = userInput;
-        // uses global state saved as a const above
-        projectState.addProject(title, desc, people);
-        this.clearInputs();
-      }
+  // method to handle form submission
+  @autobind
+  private submitHandler(event: Event) {
+    // prevent default submission
+    event.preventDefault();
+    const userInput = this.gatherUserInput();
+    // checks if userInput is an array (as TS tuples - defined in the gatherUserInput method - are)
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      // uses global state saved as a const above
+      projectState.addProject(title, desc, people);
+      this.clearInputs();
     }
   }
 }
